273b5f6adca02b2cc489c0b1f97469cbc438d1b1
Leandro Jr <leandrojr70@gmail.com>
2020-10-24 15:50:28 -0300

    criação da seção featured plants

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/home/components/title_with_more_btn.dart';
 
+import 'featured_plants.dart';
 import 'header_with_searchbox.dart';
+import 'recommend_plants.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -14,115 +17,13 @@ class Body extends StatelessWidget {
         children: <Widget>[
           HeaderWithSearchBox(size: size),
           TitleWithMoreBtn(title: "Recommended", press: () {}),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              children: <Widget>[
-                RecomendPlantCard(
-                  image: "assets/images/image_1.png",
-                  title: "Samantha",
-                  country: "Russia",
-                  price: 440,
-                  press: () {},
-                ),
-                RecomendPlantCard(
-                  image: "assets/images/image_1.png",
-                  title: "Samantha",
-                  country: "Russia",
-                  price: 440,
-                  press: () {},
-                ),
-                RecomendPlantCard(
-                  image: "assets/images/image_1.png",
-                  title: "Samantha",
-                  country: "Russia",
-                  price: 440,
-                  press: () {},
-                ),
-              ],
-            ),
+          RecommendsPlants(),
+          TitleWithMoreBtn(
+            title: "Featured Plants",
+            press: () {},
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class RecomendPlantCard extends StatelessWidget {
-  const RecomendPlantCard({
-    Key key,
-    this.image,
-    this.title,
-    this.country,
-    this.price,
-    this.press,
-  }) : super(key: key);
-
-  final String image, title, country;
-  final int price;
-  final Function press;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.only(
-        left: kDefaultPadding,
-        top: kDefaultPadding / 2,
-        bottom: kDefaultPadding * 2.5,
-      ),
-      width: size.width * 0.4,
-      child: Column(
-        children: <Widget>[
-          Image.asset(image),
-          GestureDetector(
-            onTap: press,
-            child: Container(
-              padding: EdgeInsets.all(kDefaultPadding / 2),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 10),
-                    blurRadius: 50,
-                    color: kPrimaryColor.withOpacity(0.23),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: <Widget>[
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "$title\n".toUpperCase(),
-                          style: Theme.of(context).textTheme.button,
-                        ),
-                        TextSpan(
-                          text: country.toUpperCase(),
-                          style: TextStyle(
-                            color: kPrimaryColor.withOpacity(0.5),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    '\$price',
-                    style: Theme.of(context)
-                        .textTheme
-                        .button
-                        .copyWith(color: kPrimaryColor),
-                  ),
-                ],
-              ),
-            ),
-          )
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
         ],
       ),
     );
